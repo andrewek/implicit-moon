567fb84b23f3191d4d916368f708096b9c34b030
Andrew Ek <andrewek@gmail.com>
2024-12-30 23:27:33 -0600

    Eleventy Build on File-Save

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,14 @@
 import { DateTime } from "luxon"
 
 export default async function(eleventyConfig) {
+  // Use .eleventyignore in lieu of .gitignore
+  eleventyConfig.setUseGitIgnore(false);
+
+  // Pass tailwind-generated styles through
+  eleventyConfig.addPassthroughCopy({
+    "./assets/out.css": "style.css"
+  })
+
   eleventyConfig.addFilter("humanizedDate", (dateObj) => {
     // Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("LLLL d, yyyy");
